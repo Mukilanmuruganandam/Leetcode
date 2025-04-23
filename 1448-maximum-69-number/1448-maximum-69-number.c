99a3394dece328b7239c6e1ec892faec03acd560
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-04-23 11:18:49 +0530

    Time: 0 ms (100%), Space: 8 MB (25.17%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.c
+++ 1448-maximum-69-number/1448-maximum-69-number.c
@@ -1,18 +1,18 @@
 int maximum69Number(int n)
 {
-int c=-1;
-int c1=0;
-int temp=n;
-while(temp>0)
-{
-int c2=temp%10;
-if(c2==6)
-c=c1;
-c1++;
-temp=temp/10;
-}
-if(c==-1)
-return n;
-int ans=n+(3*pow(10,c));
-return ans;
+    int c=-1;
+    int c1=0;
+    int temp=n;
+    while(temp>0)
+    {
+        int c2=temp%10;
+        if(c2==6)
+        c=c1;
+        c1++;
+        temp=temp/10;
+    }
+    if(c==-1)
+    return n;
+    int ans=n+(3*pow(10,c));
+    return ans;
 }
