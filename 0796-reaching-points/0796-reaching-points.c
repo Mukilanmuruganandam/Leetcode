48b4b9c142dadb1e93ae39ffe6acf6d82116fa31
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-04-23 14:11:36 +0530

    Time: 0 ms (100%), Space: 7.6 MB (100%) - LeetHub

--- 0796-reaching-points/0796-reaching-points.c
+++ 0796-reaching-points/0796-reaching-points.c
@@ -1,14 +1,14 @@
 bool reachingPoints(int a,int b,int c,int d){
-while(c>a&&d>b)
+while(c>a && d>b)
 {
 if(c>d)
 c%=d;
 else 
 d%=c;
 }
-if(c==a&&d>=b&&(d-b)%a==0)
+if(c==a && d>=b && (d-b)%a==0)
 return true;
-if(d==b&&c>=a&&(c-a)%b==0)
+if(d==b && c>=a && (c-a)%b==0)
 return true;
 return false;
 }
