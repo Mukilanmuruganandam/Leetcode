8d70119fc4c9a2cc3d78870a928e035d6db855c9
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-05-15 15:54:41 +0530

    Time: 158 ms (53.24%), Space: 13.6 MB (68.96%) - LeetHub

--- 0204-count-primes/0204-count-primes.c
+++ 0204-count-primes/0204-count-primes.c
@@ -1,20 +1,20 @@
 int countPrimes(int n)
-{
+{   int c,i,j;
     if(n<=2) 
     return 0;
-    int c=0;
+    c=0;
     char p[n];
-    for(int i=2;i<n;i++) 
+    for(i=2;i<n;i++) 
     p[i]=1;
-    for(int i=2;i*i<n;i++)
+    for(i=2;i*i<n;i++)
     {
         if(p[i])
         {
-            for(int j=i*i;j<n;j+=i)
+            for(j=i*i;j<n;j+=i)
                 p[j]=0;
         }
     }
-    for(int i=2;i<n;i++)
+    for(i=2;i<n;i++)
         if(p[i]) 
         c++;
     return c;
