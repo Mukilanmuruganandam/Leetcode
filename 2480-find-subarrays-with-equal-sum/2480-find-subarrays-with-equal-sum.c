a365d6f1d74848a413b1f67edf8223c42b77b6a1
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-05-28 14:32:30 +0530

    Time: 8 ms (6.67%), Space: 8.1 MB (60%) - LeetHub

--- 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.c
+++ 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.c
@@ -1,13 +1,12 @@
 bool findSubarrays(int* a,int n) {
-		int c=0,i,j;
-		for(i=0;i<n-1;i++)
+	int c=0,i,j;
+	for(i=0;i<n-1;i++)
+    {
+		for(j=i+1;j<n-1;j++)
         {
-			for(j=i+1;j<n-1;j++)
-            {
-				if(a[j]+a[j+1]==a[i]+a[i+1]) 
-                c++;
-			}
+			if(a[j]+a[j+1]==a[i]+a[i+1]) 
+            c++;
 		}
-		return c>=1;
-    
+	}
+	return c>=1;   
 }
