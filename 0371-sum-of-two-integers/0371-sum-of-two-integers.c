15dcde0960200fc240d78371e12a601d18c017f3
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-06-04 22:32:18 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.58%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.c
@@ -0,0 +1,16 @@
+int getSum(int a,int b){
+    while(b!=0)
+    {
+        if(b>0)
+        {
+            a++;
+            b--;
+        }
+        else
+        {
+            a--;
+            b++;
+        }
+    }
+    return a;
+}
