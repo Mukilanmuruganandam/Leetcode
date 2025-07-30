5f0f47b3c8dd56302771f5c99cb47dbb2590f3af
Mukilanmuruganandam <141812438+Mukilanmuruganandam@users.noreply.github.com>
2025-07-30 12:06:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (36%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.c
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.c
@@ -1,12 +1,12 @@
 int minMoves(int* a, int n) {
-    int m=a[0],i;
-    long long s=0;
+    int i;
+    long long s=0,m=a[0];
     for(i=0;i<n;i++) 
     {
         s+=a[i];
         if(a[i]<m)
             m=a[i];
     }
-    return (int)(s-(long long)m*n);
+    return (int)(s-m*n);
     
 }
