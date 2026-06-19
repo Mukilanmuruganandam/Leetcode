26c29cdfedea3aeafe0ab3fff832853cf9083dd8
MUKILAN M <141812438+Mukilanmuruganandam@users.noreply.github.com>
2026-06-19 18:43:36 +0530

    Time: 8 ms (16.15%), Space: 44.4 MB (13.86%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        java.math.BigInteger n1=new java.math.BigInteger(num1);
+        java.math.BigInteger n2=new java.math.BigInteger(num2);
+
+
+        return n1.multiply(n2).toString();        
+    }
+}
